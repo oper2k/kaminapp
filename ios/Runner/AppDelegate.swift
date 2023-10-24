eb6d0d812befe1821b1717183fa942b3e0f158e1
oper2k <76915134+oper2k@users.noreply.github.com>
2023-10-24 15:31:07 +0300

    onesignal

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 import Flutter
+import OneSignalFramework
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,16 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+        
+        // OneSignal initialization
+        OneSignal.initialize("4d12ee8b-b110-4afa-a165-bcc2ecacbe54", withLaunchOptions: launchOptions)
+        
+        // requestPermission will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+        OneSignal.Notifications.requestPermission({ accepted in
+          print("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: true)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
